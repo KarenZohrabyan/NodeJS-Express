22807c49411a90e36455c9f7f3be9582cf3a8f28
KarenZohrabyan <zohrabyankaren96@gmail.com>
2021-02-19 17:51:29 +0400

    readStream

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const bodyParser = require('body-parser')
 const app = express();
 const articles = [{title: 'example'}];
 
 app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 app.get('/', (req, res, next) => {
     res.send(articles)
 })
 
 app.post('/article', (req, res, next) => {
-    res.send('OK');
+    articles.push(req.body)
+    res.send(req.body);
 })
 
 app.get('/user', (req, res) => {
